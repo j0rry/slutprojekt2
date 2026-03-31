cf5295eac729912d8981bc666ec44908af63ad67
Oscar Alshammar <oscar.alshammar@gmail.com>
2026-03-31 15:16:17 +0200

    First commit. började med game

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Raylib_cs;
+
+static class Game
+{
+    static public int WindowXScale { get; private set; } = 800;
+    static public int WindowYScale { get; private set; } = 800;
+    public static void Start()
+    {
+        Raylib.InitWindow(WindowXScale, WindowYScale, "Game");
+        Raylib.SetTargetFPS(60);
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+
+            Raylib.EndDrawing();
+        }
+    }
+}
